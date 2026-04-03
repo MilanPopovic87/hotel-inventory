5f47e59ac09c161684a8d8c7a52b47342e6cf6b1
Milan Popovic <popovic-milan@hotmail.com>
2026-04-03 18:04:06 +0200

    Use test profile for consistent config in CI

--- backend/hotel-inventory-backend/src/test/java/com/milanpopovic/hotelinventory/service/BookingServiceTest.java
+++ backend/hotel-inventory-backend/src/test/java/com/milanpopovic/hotelinventory/service/BookingServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
@@ -19,6 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Transactional
 @WithMockUser(username = "admin", roles = {"ADMIN"})
 public class BookingServiceTest {
--- backend/hotel-inventory-backend/src/test/java/com/milanpopovic/hotelinventory/service/RoomServiceTest.java
+++ backend/hotel-inventory-backend/src/test/java/com/milanpopovic/hotelinventory/service/RoomServiceTest.java
@@ -9,6 +9,7 @@ import com.milanpopovic.hotelinventory.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
@@ -17,6 +18,7 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class RoomServiceTest {
 
     @Autowired
